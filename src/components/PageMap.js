146be0c042590e7b8579ecdc8b21a4d790ab8e7c
Elmira <timra.work@gmail.com>
2020-11-25 21:14:28 +0500

    feat(pagemap.js): add mapbox-gl

--- src/components/PageMap.js
+++ src/components/PageMap.js
@@ -1,4 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
+import mapboxgl from 'mapbox-gl';
 
-const PageMap = () => <h1 className="title title--h1 text--center center_block">Карта</h1>;
+class PageMap extends Component {
+  componentDidMount() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoidGltcmF3b3JrMTIzIiwiYSI6ImNraHhoZjB6ODAxMnQycnM3b2lvcWlwemYifQ.vsaprJd2gGI-DvkUfwxDeA';
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/timrawork123/ckhxhn8r30ukv19khb7ah55qr',
+    });
+  }
+
+  componentWillUnmount() {
+    this.map.remove();
+  }
+
+  render() {
+    const style = {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      width: '100%',
+    };
+
+    return <div style={style} ref={(el) => (this.mapContainer = el)} />;
+  }
+}
+
 export default PageMap;
