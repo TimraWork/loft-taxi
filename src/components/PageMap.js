e561e491a92aad9f3bd8023eb64a896810ab5963
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-11-26 10:22:36 +0500

    refactor(pagemap.js): replace state component with functional (useEffect+useRef)

--- src/components/PageMap.js
+++ src/components/PageMap.js
@@ -1,29 +1,22 @@
-import React, {Component} from 'react';
+import React, {useEffect, useRef} from 'react';
 import mapboxgl from 'mapbox-gl';
 
-class PageMap extends Component {
-  componentDidMount() {
+const PageMap = () => {
+  let mapRef = useRef(null);
+
+  useEffect(() => {
     mapboxgl.accessToken = 'pk.eyJ1IjoidGltcmF3b3JrMTIzIiwiYSI6ImNraHhoZjB6ODAxMnQycnM3b2lvcWlwemYifQ.vsaprJd2gGI-DvkUfwxDeA';
-    this.map = new mapboxgl.Map({
-      container: this.mapContainer,
+    const map = new mapboxgl.Map({
+      container: mapRef,
       style: 'mapbox://styles/timrawork123/ckhxhn8r30ukv19khb7ah55qr',
     });
-  }
 
-  componentWillUnmount() {
-    this.map.remove();
-  }
-
-  render() {
-    const style = {
-      position: 'absolute',
-      top: 0,
-      bottom: 0,
-      width: '100%',
+    return () => {
+      map.remove();
     };
+  }, []);
 
-    return <div style={style} ref={(el) => (this.mapContainer = el)} />;
-  }
-}
+  return <div className="map" ref={(el) => (mapRef = el)} />;
+};
 
 export default PageMap;
