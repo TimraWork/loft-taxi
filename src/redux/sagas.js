b36144803180eaf2a47dba074ce7dab77bc6d4ad
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-12-10 23:23:39 +0500

    feat(sagas.js, loginform.jsx): add registration api request

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,6 +1,6 @@
 import {takeEvery, call, put} from 'redux-saga/effects';
-import {AUTHENTICATE, logIn, PROFILE} from './actions';
-import {serverLogin, getServerCard, serverCard} from './api';
+import {AUTHENTICATE, logIn, PROFILE, REGISTER} from './actions';
+import {serverLogin, getServerCard, serverCard, serverRegister} from './api';
 
 export function* Sagas() {
   yield takeEvery(AUTHENTICATE, function* (action) {
@@ -13,14 +13,23 @@ export function* Sagas() {
       localStorage.setItem('state', JSON.stringify({auth: {loggedIn: true, token: authenticateData.token, profile: profileData}}));
     }
   });
+
   yield takeEvery(PROFILE, function* (action) {
-    console.log('PROFILE payload sage = ', action.payload);
     const profileData = yield call(serverCard, action.payload.authToken, ...Object.values(action.payload));
     console.log('S =', profileData.success, action.payload);
     if (profileData.success) {
       localStorage.removeItem('state');
-      yield put(logIn(action.payload.authToken, action.payload));
       localStorage.setItem('state', JSON.stringify({auth: {loggedIn: true, token: action.payload.authToken, profile: action.payload}}));
+      yield put(logIn(action.payload.authToken, action.payload));
+    }
+  });
+
+  yield takeEvery(REGISTER, function* (action) {
+    const registerData = yield call(serverRegister, ...Object.values(action.payload));
+    if (registerData.success) {
+      localStorage.removeItem('state');
+      localStorage.setItem('state', JSON.stringify({auth: {loggedIn: true, token: registerData.token, profile: action.payload}}));
+      yield put(logIn(registerData.token, action.payload));
     }
   });
 } // главная сага, которая вызывает другие саги.
