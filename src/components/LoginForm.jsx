91cc81ff559225d91bd3c15b1af9a51ff7f5116e
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-12-15 16:16:31 +0500

    feat(loginform.jsx): add React Hook Form plugin

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,26 +1,49 @@
 import React from 'react';
 import {Paper, Box, Button, Typography, TextField} from '@material-ui/core';
 import {Link} from 'react-router-dom';
+import {useForm} from 'react-hook-form';
+import {connect} from 'react-redux';
+import {authenticate} from '../modules/auth/actions';
 
-export const LoginForm = ({authentificate}) => (
-  <div className="center_block">
-    <Paper style={{padding: '70px'}}>
-      <form className="form w--350" onSubmit={authentificate}>
-        <Typography variant="h1" align="center">
-          Войти
-        </Typography>
-        <TextField variant="standard" label="Email" name="email" inputProps={{type: 'email'}} required />
-        <TextField style={{marginBottom: '50px'}} variant="standard" label="Пароль" name="password" inputProps={{type: 'password'}} required />
-        <Button id="login-button" className="mb--30">
-          Войти
-        </Button>
-        <Box sx={{textAlign: 'center'}}>
-          Новый пользователь?&nbsp;
-          <Link to="/registration/" id="registration-link" href="#" color="secondary">
-            Регистрация
-          </Link>
-        </Box>
-      </form>
-    </Paper>
-  </div>
-);
+export const Form = ({authenticate}) => {
+  const {register, handleSubmit} = useForm();
+
+  const onSubmit = (data) => {
+    console.log('data = ', data);
+    const {email, password} = data;
+    authenticate(email, password);
+  };
+
+  return (
+    <div className="center_block">
+      <Paper style={{padding: '70px'}}>
+        <form className="form w--350" onSubmit={handleSubmit(onSubmit)}>
+          <Typography variant="h1" align="center">
+            Войти
+          </Typography>
+          <TextField inputRef={register} variant="standard" label="Email" name="email" inputProps={{type: 'email'}} required />
+          <TextField
+            inputRef={register}
+            style={{marginBottom: '50px'}}
+            variant="standard"
+            label="Пароль"
+            name="password"
+            inputProps={{type: 'password'}}
+            required
+          />
+          <Button id="login-button" className="mb--30">
+            Войти
+          </Button>
+          <Box sx={{textAlign: 'center'}}>
+            Новый пользователь?&nbsp;
+            <Link to="/registration/" id="registration-link" href="#" color="secondary">
+              Регистрация
+            </Link>
+          </Box>
+        </form>
+      </Paper>
+    </div>
+  );
+};
+
+export const LoginForm = connect(null, {authenticate})(Form);
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
-import {authenticate} from '../modules/auth/actions';
-
 import {LoginForm} from '../components/LoginForm';
 
-export const Login = ({isLoggedIn, authenticate}) => {
-  const authentificate = (e) => {
-    e.preventDefault();
-    const {email, password} = e.target;
-    authenticate(email.value, password.value);
-  };
-
-  return <>{isLoggedIn ? <Redirect to="/map/" /> : <LoginForm authentificate={authentificate} />}</>;
+export const Login = ({isLoggedIn}) => {
+  return <>{isLoggedIn ? <Redirect to="/map/" /> : <LoginForm />}</>;
 };
 
-export const LoginWithAuth = connect((state) => ({isLoggedIn: state.auth.isLoggedIn}), {authenticate})(Login);
+export const LoginWithAuth = connect((state) => ({isLoggedIn: state.auth.isLoggedIn}))(Login);
